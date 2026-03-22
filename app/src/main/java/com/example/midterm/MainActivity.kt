6ec1e4525271bebb03d492d32c6244b08b068b91
ilay2004@bu.edu <ilay2004@bu.edu>
2026-03-22 17:03:17 -0400

    Implemented Q1

--- app/src/main/java/com/example/midterm/MainActivity.kt
+++ app/src/main/java/com/example/midterm/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.midterm.ui.theme.MidtermTheme
@@ -19,29 +25,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MidtermTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ButtonScreen()
                 }
             }
         }
-    }
 }
 
+/*Q1 Compose State & Recomposition*/
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ButtonScreen() {
+    // rememberSaveable to survive screen rotation
+    var count by rememberSaveable { mutableStateOf(0) }
+    // Button to increment the count
+    Column() {
+        Button(
+            onClick = { count += 1 }
+        ) {
+            Text("Clicked $count times")
+        }
+        // Button to reset the count
+        Button(
+            onClick = { count = 0 }
+        ){
+            Text("Reset count")
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ButtonScreenPreview() {
     MidtermTheme {
-        Greeting("Android")
+        ButtonScreen()
     }
 }
+
+/*Q2 ViewModel & Unidirectional DataFlow */
